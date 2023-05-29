08f095e9cdf7c5ad5b00f123b91ebcb95dc06ef4
cecavi <100417732+cecavi@users.noreply.github.com>
2023-05-29 15:23:41 +0200

    added packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NotFound from 'components/NotFound';
-import Main from 'components/Main';
-import Login from 'components/Login';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import user from 'reducers/user';
-import secrets from 'reducers/secrets';
-import { Provider } from 'react-redux';
 
 export const App = () => {
-
-  const reducer = combineReducers({
-    user: user.reducer,
-    secrets: secrets.reducer
-  });
-  const store = configureStore({reducer})
-
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/login' element={<Login/>}> </Route>
-          <Route path='/' element={<Main/>}> </Route>
-          <Route path='*' element={<NotFound/>}> </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <div>
+      Find me in src/app.js!
+    </div>
   );
 }
-
-/// npm i react-redux
-/// npm i @reduxjs/toolkit
-/// npm i react-router-dom
--- frontend/src/utils/urls.js
+++
@@ -1,2 +0,0 @@
-const BASE_URL = "https://project-auth-vcssurlyqq-ez.a.run.app/";
-export const API_URL = (slug) => `${BASE_URL}/${slug}`
