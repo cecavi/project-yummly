51f7c82ded6142a1cc5dc554831a44163919d7b1
cecavi <100417732+cecavi@users.noreply.github.com>
2023-06-02 12:14:06 +0200

    fix

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://project-yummly-potyd2almq-no.a.run.app";
+const BASE_URL = "https://project-yummly-potyd2almq-no.a.run.app/";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
